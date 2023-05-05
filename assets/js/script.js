6aced46afef75ea7f65fd362c98c0b44424a522f
Pimmz <>
2023-05-05 09:05:53 +0000

    Added progrees bar to time play

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+//Timer area
+function move() {
+    const element = document.getElementById("myBar");   
+    let width = 0;
+    const id = setInterval(frame, 1000);
+    function frame() {
+      if (width == 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        element.style.width = width + '%'; 
+      }
+    }
+  }
+//Game area for cards
 var cards = document.querySelectorAll('.card');
 
 [...cards].forEach((card)=>{
