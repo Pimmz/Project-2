ce1d9a119d9d74b6e43c32523c5bf00683630be0
Pimmz <>
2023-05-05 16:12:25 +0000

    Added code to enable the cards to match

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,11 +13,96 @@ function card() {
     }
 }
 
-//Game area for cards
-var cards = document.querySelectorAll('.card');
+//allows the cards to match correctly
+const cards = document.querySelectorAll(".card");
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+    this.classList.add("is-flipped");
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+    secondCard = this;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('is-flipped');
+        secondCard.classList.remove('is-flipped');
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+//Game area for cards so they flip over
+
+var cards2 = document.querySelectorAll('.card');
+
+[...cards2].forEach((card) => {
+    card.addEventListener('click', function () {
+        card.classList.toggle('is-flipped');
+    });
+});
+
+//Timer area
+
+function cards() {
+    const element = document.getElementById("myBar");
+    let width = 0;
+    const id = setInterval(frame, 600);
+    function frame() {
+        if (width == 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            element.style.width = width + '%';
+        }
+    }
+}
+
+
+//Game area for cards so they flip over
+
+var card = document.querySelectorAll('.card');
 
 [...cards].forEach((card) => {
     card.addEventListener('click', function () {
         card.classList.toggle('is-flipped');
     });
 });
+
+
+
+
+
+
+//How to play button
